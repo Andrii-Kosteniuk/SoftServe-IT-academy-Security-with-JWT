ce11cb559e25d74e5486ccbaf4a47f276620dbcc
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-12 08:32:32 +0100

    Refactored method login and created method register

--- src/main/java/com/softserve/itacademy/todolist/controller/AuthController.java
+++ src/main/java/com/softserve/itacademy/todolist/controller/AuthController.java
@@ -1,43 +1,54 @@
 package com.softserve.itacademy.todolist.controller;
 
-import com.softserve.itacademy.todolist.config.auth.JwtUtils;
-import com.softserve.itacademy.todolist.config.authDto.LoginRequest;
+import com.softserve.itacademy.todolist.config.auth.AuthenticationService;
 import com.softserve.itacademy.todolist.config.authDto.AuthenticationResponse;
+import com.softserve.itacademy.todolist.dto.user.UserRequestDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtils jwtUtils;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> login(@RequestBody UserRequestDto userRequestDto) {
         try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-
-            UserDetails user = (UserDetails) authentication.getPrincipal();
-            String token = jwtUtils.generateTokenFromUserName(user.getUsername());
-
-            AuthenticationResponse authenticationResponse = new AuthenticationResponse(
-                    user.getUsername(), token);
+            AuthenticationResponse authenticationResponse = authenticationService.authenticate(userRequestDto);
             return ResponseEntity.ok().body(authenticationResponse);
-        } catch (BadCredentialsException e) {
+        } catch (
+                BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody UserRequestDto userRequestDto) {
+        try {
+            AuthenticationResponse authenticationResponse = authenticationService.register(userRequestDto);
+            return ResponseEntity.ok().body(authenticationResponse);
+        } catch (
+                BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request,
+                             HttpServletResponse response) throws IOException {
+        authenticationService.refreshToken(request, response);
+
     }
 }
