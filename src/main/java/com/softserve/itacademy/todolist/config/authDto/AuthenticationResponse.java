7384a21d4dfd37fca1c5e301af33504f25df2645
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-12 08:36:59 +0100

    Added field for saving refresh token 'String refreshToken';

--- src/main/java/com/softserve/itacademy/todolist/config/authDto/AuthenticationResponse.java
+++ src/main/java/com/softserve/itacademy/todolist/config/authDto/AuthenticationResponse.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Component;
 public class AuthenticationResponse {
     private String username;
     private String token;
+    private String refreshToken;
 }
