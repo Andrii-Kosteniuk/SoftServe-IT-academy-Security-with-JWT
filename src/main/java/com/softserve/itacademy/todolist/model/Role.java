80e2b037cdbfb55df950f1cc15c21c857d10ea5d
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-12-12 08:27:36 +0100

    Added constructors to Role class and created RoleRepository

--- src/main/java/com/softserve/itacademy/todolist/model/Role.java
+++ src/main/java/com/softserve/itacademy/todolist/model/Role.java
@@ -10,16 +10,29 @@ import org.springframework.security.core.GrantedAuthority;
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotBlank(message = "The 'name' cannot be empty")
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
+    public Role() {
+
+    }
+
 
     @Override
     public String getAuthority() {
         return "ROLE_" + name;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
     }
 }
